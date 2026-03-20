fae991c4783a8b3937a898b5d0bba168ca0222e0
Momin Hosan Emon <mominhasanemon@gmail.com>
2026-03-20 19:52:33 +0600

    Card payment mathod added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4205138530" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2292045759" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
